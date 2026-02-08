0286d32ac26034d0ebdaa00a3a8d9715ec809c83
jhoujhou <a01450887@gmail.com>
2026-02-08 21:20:20 +0800

    上傳作業

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/weektwo-restfulapi-project/',
+  base: '/weekthree-restfulapi-project/',
   plugins: [react()],
 })
